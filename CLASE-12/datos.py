3bf04de134fc8afab04a2646c5b60ef51addce56
cvkz <134543105+cvkz@users.noreply.github.com>
2023-05-24 14:34:02 -0400

    Archivo datos.py sin validar

---
+++ CLASE-12/datos.py
@@ -0,0 +1,10 @@
+# EJEMPLO DE USO DE REPOSITORIO
+
+print("DATOS PERSONALES")
+print("-----------------------")
+vnom=input("Ingrese su nombre:  ")
+vedad=int(input("Ingrese su edad:  "))
+print("-----------------------")
+print(f"Su nombre es: {vnom}")
+print(f"Su edad es: {vedad}")
+print("Programa finalizado...")
